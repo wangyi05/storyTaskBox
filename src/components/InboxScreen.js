3c39ce0e7ccea23f3f929311b548c8641d478cc5
wangyi05 <ywang101@stevens.edu>
2020-05-06 04:23:39 -0400

    taskbox init

---
+++ src/components/InboxScreen.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import TaskList from './TaskList';
+
+export function PureInboxScreen({ error }) {
+	
+  if (error) {
+    return (
+      <div className="page lists-show">
+        <div className="wrapper-message">
+          <span className="icon-face-sad" />
+          <div className="title-message">Oh no!</div>
+          <div className="subtitle-message">Something went wrong</div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+
+    <div className="page lists-show">
+      <nav>
+        <h1 className="title-page">
+          <span className="title-wrapper">Taskbox</span>
+        </h1>
+      </nav>
+      <TaskList tasks={[]}/>
+    </div>
+  );
+}
+
+PureInboxScreen.propTypes = {
+  error: PropTypes.string,
+};
+
+PureInboxScreen.defaultProps = {
+  error: null,
+};
+
+const mapStateToProps = error => {
+	return {
+		error : error,
+	}
+}
+
+export default connect(mapStateToProps)(PureInboxScreen);
